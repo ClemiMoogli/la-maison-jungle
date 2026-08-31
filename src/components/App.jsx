c8d439d3d6d35703a61ed7ceb57f639f846d754e
Clement <clementjeannin90@gmail.com>
2026-08-31 21:58:03 +0200

    en attente de l integration du logo

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Banner from './Banner.jsx'
+import Cart from './Cart.jsx'
+
+function App() {
+  return (
+  <>
+    <Banner title="La maison jungle"/>
+    <Cart />
+  </>
+  );
+  
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import '../styles/Banner.css'
+
+function Banner({title}) {
+  return (
+    <>
+1   <div className="banner">
+      <h1 className="banner-title">{title}</h1>
+    </div>
+    </>
+  );
+}
+
+export default Banner
